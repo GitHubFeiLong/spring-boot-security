5079b1da1fe0c8cb669f202f40cdfd8c64f1e26b
MSI <169madman@gmail.com>
2020-12-27 15:18:54 +0800

    init

--- src/main/java/com/cfl/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/cfl/springsecurity/SpringSecurityApplication.java
@@ -2,8 +2,23 @@ package com.cfl.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
+/**
+ * 类描述：
+ * EnableGlobalMethodSecurity: 第一步是启用方法安全性
+ * 第二步：修饰方法资源
+ *   @Secured("ROLE_USER")
+ *   public String secure() {
+ *     return "Hello Security";
+ *   }
+ * @ClassName SpringSecurityApplication
+ * @Author msi
+ * @Date 2020/12/27 15:14
+ * @Version 1.0
+ */
 @SpringBootApplication
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SpringSecurityApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/cfl/springsecurity/config/WebSecurityConfig.java
@@ -0,0 +1,77 @@
+package com.cfl.springsecurity.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.sql.DataSource;
+
+/**
+ * 类描述：
+ * 使用@enablewebsecurity 注释，以启用 Spring Security 的 web 安全支持并提供 Spring MVC 集成。
+ * 它还扩展了 WebSecurityConfigurerAdapter 并重写了它的一些方法，以设置 web 安全配置的一些细节
+ * @ClassName WebSecurityConfig
+ * @Description TODO
+ * @Author msi
+ * @Date 2020/12/27 14:47
+ * @Version 1.0
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+    /**
+     * 方法定义了哪些 URL 路径应该受到保护，哪些不应该受到保护。具体来说,/和/home 路径被配置为不需要任何身份验证。
+     * 所有其他路径都必须经过身份验证。
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+        auth.jdbcAuthentication().dataSource(dataSource).withUser("dave")
+                .password("secret").roles("USER");
+    }
+
+    /**
+     * 用单个用户设置内存中的用户存储。该用户获得用户名、密码密码和 USER 角色。
+     * @return
+     */
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
